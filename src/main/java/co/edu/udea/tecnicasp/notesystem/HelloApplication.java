2de10017f543ba0f80b9886b012eb7b18c0449c4
Jonatan <elfokincrack@gmail.com>
2022-03-30 22:00:07 -0500

    se le agrego estilo a la wiew calculo-rapido

--- src/main/java/co/edu/udea/tecnicasp/notesystem/HelloApplication.java
+++ src/main/java/co/edu/udea/tecnicasp/notesystem/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/contenedor-principal.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 450);
+        stage.setTitle("Notes System");
         stage.setScene(scene);
         stage.show();
     }
